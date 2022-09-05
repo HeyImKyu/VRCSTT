b7f1f6619892ef7d63eddbdc2b164fd04ddddae0
Leone-kyun <3ad4gum@gmail.com>
2022-09-05 11:46:27 +0200

    init

---
+++ Config/STTConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VRCSTT.Config
+{
+    internal static class STTConfig
+    {
+        internal static string SubscriptionKey = "f5241ab63dea4f008e8c04dfab3420ce";
+        internal static string Region = "germanywestcentral";
+
+        internal static string Address = "127.0.0.1";
+        internal static int Port = 9000;
+    }
+}
---
+++ OscClient.cs
@@ -0,0 +1,34 @@
+﻿using OscCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace VRCSTT
+{
+    internal class OscClient
+    {
+        private readonly UdpClient client;
+        private readonly CancellationTokenSource cts;
+
+        public event EventHandler<OscPacket> PacketReceived;
+
+        public OscClient(string hostName, int port)
+        {
+            client = new UdpClient();
+            client.Connect(hostName, port);
+            cts = new CancellationTokenSource();
+        }
+
+        public Task SendAsync(OscPacket packet)
+        {
+            var data = packet.ToByteArray();
+            return client.SendAsync(data, data.Length);
+        }
+
+        public void Dispose() => cts.Cancel();
+    }
+}
