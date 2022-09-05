1c50a52dba2f6c261db996d3d6202e4d3a9cd642
Leone-kyun <3ad4gum@gmail.com>
2022-09-05 18:48:06 +0200

    Added files to new master

---
+++ Config/STTConfig.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VRCSTT.Config
+{
+    internal static class STTConfig
+    {
+        private static ConfigProvider instance;
+
+        internal static string SubscriptionKey => GetSubscriptionKey();
+        internal static string Region => GetRegionKey(); 
+
+        internal static string Address = "127.0.0.1";
+        internal static int Port = 9000;
+
+        private static string GetSubscriptionKey()
+        {
+            CreateInstance();
+            return instance.GetSubscriptionKey();
+        }
+
+        private static string GetRegionKey()
+        {
+            CreateInstance();
+            return instance.GetRegion();
+        }
+
+        private static void CreateInstance()
+        {
+            if (instance == null)
+                instance = new ConfigProvider();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+using OscCore;
+using OscCore.LowLevel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using VRCSTT.Config;
+
+namespace VRCSTT
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string[] Languages
+        {
+            get
+            {
+                return new string[] { "en-US", "de-DE", "fi-FI" };
+            }
+        }
+
+        private void SetTextboxText(string text)
+        {
+            OutputTextbox.Text = text;
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+            LanguageBox.SelectedIndex = 0;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OSCHandler.IsTyping(true);
+            SetTextboxText(STTHandler.StartSpeaking(LanguageBox.SelectedItem.ToString()));
+        }
+    }
+}
