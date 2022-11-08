cbfa13aabf2f11c252a9df1e0cbf0a54730494b7
Kyu ♥ <3ad4gum@gmail.com>
2022-11-08 12:00:12 +0100

    Implemented most of this stuff

---
+++ Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Data;
+
+namespace VRCSTT.Converters
+{
+    [ValueConversion(typeof(bool), typeof(bool))]
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            if ((bool)value)
+            {
+                return Visibility.Visible;
+            }
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+
+        #endregion
+    }
+}
